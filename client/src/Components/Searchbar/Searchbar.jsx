21d50a0af0605806cdd0b53ba4b595444bf2c891
yirano <yirano@protonmail.ch>
2020-05-25 19:40:43 -0400

    Add placeholder for location

--- client/src/Components/Searchbar/Searchbar.jsx
+++ client/src/Components/Searchbar/Searchbar.jsx
@@ -6,7 +6,7 @@ const Searchbar = (props) => {
   return (
     <Form onSubmit={e => props.handleSubmit(e)} className="form">
       <Input type="text" name="search" placeholder="Job Title" value={props.jobTitle} onChange={e => props.handleChange(e)} value={props.search} />
-      <Input type="text" name="location" value={props.location} onChange={e => props.handleChange(e)} />
+      <Input type="text" name="location" value={props.location} onChange={e => props.handleChange(e)} placeholder="Location" />
       <Input type="submit" className="button" />
     </Form>
   )
